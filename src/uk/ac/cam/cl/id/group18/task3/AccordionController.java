d657776eecb70c4efc83dc4b70c9f228f10648b9
Charles J.Y. Yoon <jyy24@cam.ac.uk>
2017-05-16 14:39:42 +0100

    Splitting the panes.

---
+++ src/uk/ac/cam/cl/id/group18/task3/AccordionController.java
@@ -0,0 +1,7 @@
+package uk.ac.cam.cl.id.group18.task3;
+
+/**
+ * Created by jaeyeun on 17. 5. 16.
+ */
+public class AccordionController {
+}
--- src/uk/ac/cam/cl/id/group18/task3/Main.java
+++ src/uk/ac/cam/cl/id/group18/task3/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,6 +12,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("task3.fxml"));
+        Parent accordion = FXMLLoader.load(getClass().getResource("accordion.fxml"));
+        Parent map = FXMLLoader.load(getClass().getResource("map.fxml"));
+        ((GridPane) root).add(map, 0, 0);
+        ((GridPane) root).add(accordion, 1, 0);
 
         primaryStage.setTitle("Group 18 Weather App");
         primaryStage.setScene(new Scene(root, 1280, 800));
