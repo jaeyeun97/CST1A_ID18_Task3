51d0448f7ac04d978398826495277f9f016d4554
LemonWaterr <1004wsh@gmail.com>
2017-05-18 15:08:26 +0100

    Better Accordion

--- src/uk/ac/cam/cl/id/group18/task3/AccordionController.java
+++ src/uk/ac/cam/cl/id/group18/task3/AccordionController.java
@@ -18,30 +18,42 @@ public class AccordionController {
     @FXML
     private Accordion accordion;
     private Image sunnyCloud = new Image("https://cdn.pixabay.com/photo/2012/04/18/13/21/clouds-37009_960_720.png", 0, 24, true, true);
-    
-    private TitledPane Accord(Image weather){
+    private Label Monday = new Label("Monday");
+    private Label Tuesday = new Label("Tuesday");
+    		
+    private TitledPane Accord(Label day, Image weather){
 		TitledPane oneTab = new TitledPane();
-		Label day = new Label("Monday");
 		
-		VBox row = new VBox();
-		row.setStyle("-fx-padding: 10");
-		row.setSpacing(10);
-		row.getChildren().add(new Label("weatherInfo 1"));
+		VBox mainBox = new VBox();
+		mainBox.setStyle("-fx-padding: 10");
+		mainBox.setSpacing(10);
+		mainBox.getChildren().add(day);
 	
-	    HBox col = new HBox();
-	    col.setSpacing(3);	
-	    col.setAlignment(Pos.CENTER);
-	    col.getChildren().add(new ImageView(weather));
-	    col.getChildren().add(day);
+	    HBox subBox = new HBox();
+	    subBox.setSpacing(10);
+	    subBox.setAlignment(Pos.CENTER);
+	    subBox.getChildren().add(new ImageView(weather));
+	    subBox.getChildren().add(new Label("33¡ÆC"));
+	    subBox.getChildren().add(new ImageView(weather));
+	    subBox.getChildren().add(new Label("100mph"));
+	    subBox.getChildren().add(new ImageView(weather));
+	    subBox.getChildren().add(new Label("NW"));
+	   
 	    
-	    row.getChildren().add(col);
-	    
-		//oneTab.setGraphic(header);
-	    oneTab.setGraphic(row);
+	    mainBox.getChildren().add(subBox);
+	    oneTab.setGraphic(mainBox);
 		return oneTab;
     }
     @FXML
     private void initialize(){
-        accordion.getPanes().add(Accord(sunnyCloud));
+    	TitledPane day1 = Accord(Monday, sunnyCloud);
+    	day1.setContent(new Label("test pane, would be replaced with anchorpane"));
+    	
+    	TitledPane day2 = Accord(Tuesday, sunnyCloud);
+    	day2.setContent(new Label("test pane, would be replaced with anchorpane"));
+    	
+        accordion.getPanes().add(day1);
+        accordion.getPanes().add(day2);
+        
     }
 }
