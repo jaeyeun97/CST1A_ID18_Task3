dc332d9aae2311493ab905de2991d44660e4b0a4
LemonWaterr <1004wsh@gmail.com>
2017-05-18 16:05:12 +0100

    Better Accord 2

--- src/uk/ac/cam/cl/id/group18/task3/AccordionController.java
+++ src/uk/ac/cam/cl/id/group18/task3/AccordionController.java
@@ -1,13 +1,19 @@
 package uk.ac.cam.cl.id.group18.task3;
 
-import javafx.beans.binding.Bindings;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -18,12 +24,12 @@ public class AccordionController {
     @FXML
     private Accordion accordion;
     private Image sunnyCloud = new Image("https://cdn.pixabay.com/photo/2012/04/18/13/21/clouds-37009_960_720.png", 0, 24, true, true);
-    private Label Monday = new Label("Monday");
-    private Label Tuesday = new Label("Tuesday");
+    private Label Mon = new Label("Monday");
+    private Label Tue = new Label("Tuesday");
+    private Label Wed = new Label("Wednesday");
     		
-    private TitledPane Accord(Label day, Image weather){
+    private TitledPane accord(Label day, Image weather){
 		TitledPane oneTab = new TitledPane();
-		
 		VBox mainBox = new VBox();
 		mainBox.setStyle("-fx-padding: 10");
 		mainBox.setSpacing(10);
@@ -44,16 +50,53 @@ public class AccordionController {
 	    oneTab.setGraphic(mainBox);
 		return oneTab;
     }
+    
+    private ScrollPane hourly(){
+    	ScrollPane base = new ScrollPane();
+    	
+    	List<VBox> hours = new ArrayList<>();
+    	for(int i=0; i<8; i++){
+    		VBox hour = new VBox();
+    		hour.setSpacing(5);
+    		List<Node> contents = hour.getChildren();
+    		contents.add(new Label("hourly temp"));
+    		contents.add(new Label("hourly wind"));
+    		contents.add(new Label("sth els"));
+    	}
+    	
+    	HBox scrollContent = new HBox();
+    	scrollContent.getChildren().addAll(hours);
+    	
+    	base.setContent(scrollContent);
+    	base.setHbarPolicy(ScrollBarPolicy.NEVER);
+    	base.setMaxSize(300, 150);
+    	
+    	return base;
+    }
+    
     @FXML
     private void initialize(){
-    	TitledPane day1 = Accord(Monday, sunnyCloud);
-    	day1.setContent(new Label("test pane, would be replaced with anchorpane"));
+
+    	TitledPane day1 = accord(Mon, sunnyCloud);
+    	GridPane temp = new GridPane();
+    	temp.add(hourly(), 0, 1);
+    	day1.setContent(temp);
     	
-    	TitledPane day2 = Accord(Tuesday, sunnyCloud);
+    	TitledPane day2 = accord(Tue, sunnyCloud);
     	day2.setContent(new Label("test pane, would be replaced with anchorpane"));
+    	
+    	TitledPane day3 = accord(Wed, sunnyCloud);
+    	day2.setContent(new Label("test pane, would be replaced with anchorpane"));
+    	
+
     	
         accordion.getPanes().add(day1);
         accordion.getPanes().add(day2);
+        accordion.getPanes().add(day3);
+        //accordion.getPanes().add(day4);
+        //accordion.getPanes().add(day5);
+        //accordion.getPanes().add(day6);
+        //accordion.getPanes().add(day7);
         
     }
 }
