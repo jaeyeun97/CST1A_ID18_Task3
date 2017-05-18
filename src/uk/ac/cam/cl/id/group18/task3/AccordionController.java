7305a76b5af7e342415ed053debfdcaa4dc52b5b
LemonWaterr <1004wsh@gmail.com>
2017-05-18 14:15:13 +0100

    Accordion Icon + css
    Putting icon in accordin & tried css but doesnt rly work lol

--- src/uk/ac/cam/cl/id/group18/task3/AccordionController.java
+++ src/uk/ac/cam/cl/id/group18/task3/AccordionController.java
@@ -1,8 +1,15 @@
 package uk.ac.cam.cl.id.group18.task3;
 
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * Created by jaeyeun on 17. 5. 16.
@@ -10,9 +17,32 @@ import javafx.scene.control.Label;
 public class AccordionController {
     @FXML
     private Accordion accordion;
-
+    
+    private TitledPane Accord(){
+		TitledPane oneTab = new TitledPane();
+		Label day = new Label("Monday");
+		Image weather = new Image( "https://cdn.pixabay.com/photo/2012/04/18/13/21/clouds-37009_960_720.png", 0, 24, true, true);
+		
+		VBox vPane = new VBox();
+		vPane.setStyle("-fx-padding: 10");
+		vPane.setSpacing(10);
+		vPane.getChildren().add(new Label("weatherInfo 1"));
+	
+		Label someLabel = new Label();
+	    someLabel.textProperty().bind(Bindings.size(vPane.getChildren()).asString());
+	
+	    HBox header = new HBox();
+	    header.setSpacing(3);
+	    header.setAlignment(Pos.CENTER);
+	    header.setStyle("header");
+	    header.getChildren().add(someLabel);
+	    header.getChildren().add(new ImageView(weather));
+	    header.getChildren().add(day);
+		oneTab.setGraphic(header);
+		return oneTab;
+    }
     @FXML
     private void initialize(){
-        accordion.getPanes().get(0).setGraphic(new Label("Google"));
+        accordion.getPanes().add(Accord());
     }
 }
--- src/uk/ac/cam/cl/id/group18/task3/Main.java
+++ src/uk/ac/cam/cl/id/group18/task3/Main.java
@@ -27,37 +27,13 @@ public class Main extends Application {
         ((GridPane) root).add(map, 0, 0);
         ((GridPane) root).add(accordion, 1, 0);
         
-        ((GridPane) root).add(accord(), 2, 0);
-
+        Scene scene = new Scene(root, 1280, 800);
+        
         primaryStage.setTitle("Group 18 Weather App");
-        primaryStage.setScene(new Scene(root, 1280, 800));
+        primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image("file:images/parachute.png"));
 
         primaryStage.show();
-    }
-
-    public TitledPane accord(){
-    	TitledPane oneTab = new TitledPane();		
-    	Label day = new Label("Monday");
-    	Image weather = new Image( "https://cdn.pixabay.com/photo/2012/04/18/13/21/clouds-37009_960_720.png", 0, 24, true, true);
-    	
-    	VBox errorPane = new VBox();
-    	errorPane.setStyle("-fx-padding: 10");
-    	errorPane.setSpacing(10);
-    	errorPane.getChildren().add(new Label("weatherInfo 1"));
-
-    	Label nErrors = new Label();
-        nErrors.getStyleClass().add("nerrors");
-        nErrors.textProperty().bind(Bindings.size(errorPane.getChildren()).asString());
-
-        HBox header = new HBox();
-        header.setSpacing(2);
-        header.setAlignment(Pos.CENTER);
-        header.setStyle("header");
-        header.getChildren().add(nErrors);
-        header.getChildren().add(new ImageView(weather));
-        header.getChildren().add(day);
-		oneTab.setGraphic(header);
-		return oneTab;
     }
 
     public static void main(String[] args) {
