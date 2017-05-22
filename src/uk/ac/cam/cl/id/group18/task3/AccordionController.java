3dd489522146d58d3e4192a031927608b078d0c6
iunodora <tiz20@cam.ac.uk>
2017-05-23 00:42:48 +0100

    New tabs - just skeleton

--- src/uk/ac/cam/cl/id/group18/task3/AccordionController.java
+++ src/uk/ac/cam/cl/id/group18/task3/AccordionController.java
@@ -6,11 +6,8 @@ import java.util.List;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
-import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Border;
@@ -24,6 +21,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * Created by jaeyeun on 17. 5. 16.
@@ -56,7 +54,7 @@ public class AccordionController {
 	    subBox.setSpacing(10);
 	    subBox.setAlignment(Pos.CENTER);
 	    subBox.getChildren().add(new ImageView(weather));
-	    subBox.getChildren().add(new Label("33¨¬C"));
+	    subBox.getChildren().add(new Label("33ÂºC"));
 	    subBox.getChildren().add(new ImageView(weather));
 	    subBox.getChildren().add(new Label("100mph"));
 	    subBox.getChildren().add(new ImageView(weather));
@@ -68,62 +66,30 @@ public class AccordionController {
 		return oneTab;
     }
 
-    private ScrollPane hourly(){
-    	ScrollPane base = new ScrollPane();
-    	base.setPannable(true);
-    	base.setId("accord");
-
-    	List<VBox> hours = new ArrayList<>();
-    	for(int i=0; i<8; i++){
-    		VBox hour = new VBox();
-    		hour.setId("accord");
-    		hour.setSpacing(5);
-
-    		List<Node> contents = hour.getChildren();
-    		contents.add(new Label("00:00"));
-    		contents.add(new Label("10 ¨¬C"));
-    		contents.add(new Label("999 mph"));
-    		hours.add(hour);
-    	}
-
-    	HBox scrollContent = new HBox();
-    	scrollContent.setId("accord");
-    	scrollContent.setSpacing(3);
-    	scrollContent.getChildren().addAll(hours);
-
-    	base.setContent(scrollContent);
-    	base.setHbarPolicy(ScrollBarPolicy.NEVER);
-    	base.setVbarPolicy(ScrollBarPolicy.NEVER);
-
-    	return base;
-    }
 
     @FXML
     private void initialize(){
 
     	TitledPane day1 = accord(Mon, sunnyCloud);
-    	GridPane temp = new GridPane();
-    	temp.add(hourly(), 0, 0);
-    	temp.add(accord_content(), 0, 1);
-    	day1.setContent(temp);
+    	day1.setContent(content());
     	
     	TitledPane day2 = accord(Tue, sunnyCloud);
-    	day2.setContent(accord_content());
+    	day2.setContent(content());
     	
     	TitledPane day3 = accord(Wed, sunnyCloud);
-    	day3.setContent(accord_content());
+    	day3.setContent(content());
 
     	TitledPane day4 = accord(Thu, sunnyCloud);
-    	day4.setContent(accord_content());
+    	day4.setContent(content());
 
     	TitledPane day5 = accord(Fri, sunnyCloud);
-    	day5.setContent(accord_content());
+    	day5.setContent(content());
 
     	TitledPane day6 = accord(Sat, sunnyCloud);
-    	day6.setContent(accord_content());
+    	day6.setContent(content());
 
     	TitledPane day7 = accord(Sun, sunnyCloud);
-    	day7.setContent(accord_content());
+    	day7.setContent(content());
 
 
         accordion.getPanes().add(day1);
@@ -136,8 +102,138 @@ public class AccordionController {
     }
 
 
+    private ScrollPane content(){
+		ScrollPane base = new ScrollPane();
+		base.setPannable(true);
+
+		List<VBox> hours = new ArrayList<>();
+		for(int i=0; i<8; i++){
+			VBox hour = new VBox();
+			hour.getStyleClass().add("hour");
+			List<Node> content = hour.getChildren();
+
+			HBox wind = new HBox();
+			ImageView windimg = new ImageView();
+			windimg.setImage(new Image("file:images/wind.png"));
+			windimg.getStyleClass().add("windicon");
+			wind.getChildren().add(windimg);
+			Label wind_val = new Label("7-9");
+			wind_val.getStyleClass().add("wind_val");
+			wind.getChildren().add(wind_val);
+;			Label wind_msmt = new Label("mph");
+			wind_msmt.getStyleClass().add("wind_msmt");
+			wind.getChildren().add(wind_msmt);
+			Label wind_dir = new Label("NE");
+			wind_dir.getStyleClass().add("wind_dir");
+			wind.getChildren().add(wind_dir);
+			content.add(wind);
+
+
+			HBox temp = new HBox();
+			ImageView tempimg = new ImageView();
+			tempimg.setImage(new Image("file:images/temp.png"));
+			tempimg.getStyleClass().add("tempicon");
+			temp.getChildren().add(tempimg);
+			Label temp_val = new Label("11");
+			temp_val.getStyleClass().add("temp_val");
+			temp.getChildren().add(temp_val);
+			Label temp_msmt = new Label("ÂºC");
+			temp_msmt.getStyleClass().add("temp_msmt");
+			temp.getChildren().add(temp_msmt);
+			content.add(temp);
+
+
+			HBox prec = new HBox();
+			ImageView precimg = new ImageView();
+			precimg.setImage(new Image("file:images/prec.png"));
+			precimg.getStyleClass().add("precicon");
+			prec.getChildren().add(precimg);
+			Label prec_val = new Label("0%");
+			prec_val.getStyleClass().add("small_val");
+			prec.getChildren().add(prec_val);
+			content.add(prec);
+
+
+			HBox hum = new HBox();
+			ImageView humimg = new ImageView();
+			humimg.setImage(new Image("file:images/hum.png"));
+			humimg.getStyleClass().add("humicon");
+			hum.getChildren().add(humimg);
+			Label hum_val = new Label("0%");
+			hum_val.getStyleClass().add("small_val");
+			hum.getChildren().add(hum_val);
+			content.add(hum);
+
+
+			HBox uv = new HBox();
+			ImageView uvimg = new ImageView();
+			uvimg.setImage(new Image("file:images/uv.png"));
+			uvimg.getStyleClass().add("uvicon");
+			uv.getChildren().add(uvimg);
+			Label uv_val = new Label("6");
+			uv_val.getStyleClass().add("small_val");
+			uv.getChildren().add(uv_val);
+			content.add(uv);
+
+
+			HBox pres = new HBox();
+			ImageView presimg = new ImageView();
+			presimg.setImage(new Image("file:images/pres.png"));
+			presimg.getStyleClass().add("presicon");
+			pres.getChildren().add(presimg);
+			Label pres_val = new Label("1022 hPa");
+			pres_val.getStyleClass().add("small_val");
+			pres.getChildren().add(pres_val);
+			content.add(pres);
+
+
+
+			hour.setMinWidth(320);
+			hours.add(hour);
+		}
+
+		HBox scrollContent = new HBox();
+		scrollContent.setSpacing(4);
+		scrollContent.getChildren().addAll(hours);
+
+		base.setContent(scrollContent);
+		ScrollBar scroll = new ScrollBar();
+		base.setVbarPolicy(ScrollBarPolicy.NEVER);
+		base.setHbarPolicy(ScrollBarPolicy.NEVER);
+
+		return base;
+	}
+
+
+/**
+	private ScrollPane hourly(){
+		ScrollPane base = new ScrollPane();
+		base.setPannable(true);
+		base.setId("accord");
+
+		List<VBox> hours = new ArrayList<>();
+		for(int i=0; i<8; i++){
+			VBox hour = new VBox();
+			hour.setId("accord");
+			hour.setSpacing(5);
+
+			hours.add(hour);
+		}
+
+		HBox scrollContent = new HBox();
+		scrollContent.setId("accord");
+		scrollContent.setSpacing(3);
+		scrollContent.getChildren().addAll(hours);
+
+		base.setContent(scrollContent);
+		base.setHbarPolicy(ScrollBarPolicy.NEVER);
+		base.setVbarPolicy(ScrollBarPolicy.NEVER);
+
+		return base;
+	}
+
     @FXML
-	private GridPane accord_content (){
+	private VBox accord_content (){
     	GridPane content = new GridPane();
 		content.getStyleClass().add("content_tab");
 
@@ -155,7 +251,7 @@ public class AccordionController {
         Label temp_val = new Label("11");
 		temp_val.getStyleClass().add("temp_val");
     	content.add(temp_val, 0, 1);
-    	Label temp_msmt = new Label("¨¬C");
+    	Label temp_msmt = new Label("ÂºC");
 		temp_msmt.getStyleClass().add("temp_msmt");
     	content.add(temp_msmt, 1, 1);
 
@@ -187,7 +283,12 @@ public class AccordionController {
     	pres_val.getStyleClass().add("small_val");
     	content.add(pres_val, 1, 5);
 
-    	return content;
+    	VBox final_content = new VBox();
+    	final_content.getChildren().add(content);
+    	final_content.getChildren().add(hourly());
+
+    	return final_content;
 	}
+**/
 
 }
